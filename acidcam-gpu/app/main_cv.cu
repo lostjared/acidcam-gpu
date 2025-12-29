346567e0500d26d1b1dec4d97c612452599ca2d1
Jared Bruni <jaredbruni@gmail.com>
2025-12-28 17:44:01 -0800

    argument for  dynamic buffer size info message

--- acidcam-gpu/app/main_cv.cu
+++ acidcam-gpu/app/main_cv.cu
@@ -16,7 +16,7 @@ bool isNumeric(const std::string &text) {
 
 int main(int argc, char** argv) {
     if (argc != 4) {
-        std::cerr << "Usage: " << argv[0] << " camera_index start_filter" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " camera_index start_filter dynmic_buffer_size" << std::endl;
         return -1;
     }
     bool camera_mode = false;
