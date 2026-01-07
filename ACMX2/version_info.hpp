dd62022fdb483912311ed39451d2a0b6c3a471fd
Jared Bruni <jaredbruni@gmail.com>
2026-01-06 17:46:27 -0800

    version increase

--- ACMX2/version_info.hpp
+++ ACMX2/version_info.hpp
@@ -1,6 +1,6 @@
 #ifndef __VERSION_INFO_H_
 #define __VERSION_INFO_H_
-#define VERSION_INFO "v2.0.0"
+#define VERSION_INFO "v2.0.1"
 #define VERSION_AUTHOR "LostSideDead"
 #define PROGRAM_NAME "ACMX2"
 #endif
