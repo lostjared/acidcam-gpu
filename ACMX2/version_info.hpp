09e72ec3fa29357c8feb2d0193331dec31614680
Jared Bruni <jaredbruni@gmail.com>
2026-04-12 20:34:59 -0700

    version increase 2.7.0 rebuild MXWrite and everything to get up to date

--- ACMX2/version_info.hpp
+++ ACMX2/version_info.hpp
@@ -1,6 +1,6 @@
 #ifndef __VERSION_INFO_H_
 #define __VERSION_INFO_H_
-#define VERSION_INFO "v2.6.0"
+#define VERSION_INFO "v2.7.0"
 #define VERSION_AUTHOR "LostSideDead"
 #define PROGRAM_NAME "ACMX2"
 #endif
